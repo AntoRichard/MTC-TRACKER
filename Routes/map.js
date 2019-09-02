72819c80723e800dc01a3ab6c33e0c3aa8b0c576
AntoRichard <anto22998@gmail.com>
2019-09-02 23:49:19 +0530

    after finl update

--- Routes/map.js
+++ Routes/map.js
@@ -4,9 +4,6 @@ const fetch = require("node-fetch");
 const Route = express.Router();
 
 Route.get('/map',async (req,res)=>{
-    // fetch('https://mtc-conductor.firebaseio.com/seats.json',{method:"GET"})
-    // .then(res => res.json())
-    // .then(data =>console.log(data));
     const response = await fetch('https://mtc-conductor.firebaseio.com/seats.json',{method:"GET"});
     const data = await response.json();
     const keys = Object.keys(data).map(key => data[key]);
@@ -14,10 +11,16 @@ Route.get('/map',async (req,res)=>{
     keys.forEach(key =>{
         totalSeats+=key.value;
     });
+    totalSeats = 50-totalSeats;
+    if(totalSeats<0){
+        totalSeats = 0;
+    }
     console.log(totalSeats);
     res.render('map',{
-        seats : totalSeats
+        seats : totalSeats,
+        price : keys[(keys.length)-1].price
     });
+    // console.log(keys[(keys.length)-1].price);
 });
 
 module.exports = Route;
